3251d8044818ccd12daeda6ec761b59a51240551
rfigueror1 <rfigueror@gmail.com>
2018-06-14 14:25:17 -0700

    Initial changes

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,9 +4,33 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+  //make a node, see if 
+  //check if its the list is empty
+    //if list is empty assign the node as the head
+    //else get the tail and assigb the tail's pointer to the newly created node
+    //new node is the new tail
+    var tempNode = Node(value);
+    var newKeys = Object.keys(list);
+    var tempIndex = newKeys.length;
+    list.tempIndex = tempNode;
+    if(list.head === null) {
+      list.head = tempNode;
+      list.tail = tempNode;  
+    }else{
+      list.tail.next = tempNode;
+    }
+    list.tail = tempNode;    
   };
 
   list.removeHead = function() {
+    // remove the head node and return it
+    // get the node that the head node points to: call it targetNode
+    // set tempNode to current head
+    // set head to targetNode
+    // return tempNode
+    var tempNode = _.extend({}, list.head);
+    list.head = tempNode.next;
+    return tempNode.value;
   };
 
   list.contains = function(target) {

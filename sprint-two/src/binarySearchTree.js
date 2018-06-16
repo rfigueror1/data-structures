ccfd2c15630ebe3f9a8385b0ba2ee3f9b4657b8e
rfigueror1 <rfigueror@gmail.com>
2018-06-16 11:05:29 -0700

    Finished contains function for BinaryTree class.

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -11,28 +11,67 @@ var binarytTreeMethods = {};
 
 binarytTreeMethods.insert = function(value) {
   let parent = this;
-  if (value > parent) {
+  if (value > parent.value) {
     if(this.right === null){
+    //console.log('inside right');
       this.right = BinarySearchTree(value);
+      return;
     }else{
       // do the recursive call
-      insert.call(this, value);
+      this.right.insert(value);
       
     }
   } else { // means it's less than
     if (this.left === null) {
       this.left = BinarySearchTree(value);
+    //console.log('this.left: ', this.left);
+      return;
     } else {
-    
-      insert.call(this, value);
+      this.left.insert(value);
     }
   }
-  console.log(parent, value);
+  //console.log(parent, value);
   // console.log(value);
 }
 
 binarytTreeMethods.contains = function(value) {
+  let isFound = false;
+  let reachedEnd = false;
+  let currentNode = this;
   
+  while (!isFound || !reachedEnd ) {
+    // If the current node is empty, return early.
+    if (currentNode === null) {
+      return isFound;
+    }
+    
+    console.log('current node value: ', currentNode.value);
+
+    if (currentNode.value === value) {
+      isFound = true;
+      return true;
+    } 
+    
+    else if (currentNode.value < value) { // right side
+      console.log('going right ', 'currentNode: ', currentNode.value);
+      currentNode = currentNode.right;
+      if (currentNode === null) {
+        reachedEnd = true;
+        return false;
+      }
+    } 
+    
+    else { // left side
+      if (currentNode.value > value) {
+        currentNode = currentNode.left;
+        if (currentNode === null) {
+          reachedEnd = true;
+          return false;
+        }
+      }
+    }
+  }
+  return isFound;
 }
 
 binarytTreeMethods.depthFirstLog = function(callback) {
@@ -41,3 +80,8 @@ binarytTreeMethods.depthFirstLog = function(callback) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+ 
+ 
+ 
+ 
+ 
